6de766a7597d7c5afebb8d2ba3ef7c9879118a17
Jørgen Ringstad Møglestue <jorgen@bitfocus.io>
2025-06-02 14:22:37 +0200

    Remove mention of unsupported OBSBOT Talent

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ export function GetConfigFields(): SomeCompanionConfigField[] {
 			width: 12,
 			label: 'Information',
 			value:
-				'This module will communicate with OBSBOT products over OSC. It can be applied to the OBSBOT Center App (software), OBSBOT Tail 2 (hardware), OBSBOT Tail Air (hardware), and OBSBOT Talent (hardware). The OSC application on OBSBOT Center is compatible with the OBSBOT Tiny series (Tiny, Tiny 4K, Tiny 2, Tiny 2 Lite, Tiny SE), OBSBOT Meet series (Meet, Meet 4K, Meet 2, Meet SE), and OBSBOT Tail series (Tail Air, Tail 2). However, when using the OSC protocol with OBSBOT Center, the software must be running and OSC activated.',
+				'This module will communicate with OBSBOT products over OSC. It can be applied to the OBSBOT Center App (software), OBSBOT Tail 2 (hardware), and OBSBOT Tail Air (hardware). The OSC application on OBSBOT Center is compatible with the OBSBOT Tiny series (Tiny, Tiny 4K, Tiny 2, Tiny 2 Lite, Tiny SE), OBSBOT Meet series (Meet, Meet 4K, Meet 2, Meet SE), and OBSBOT Tail series (Tail Air, Tail 2). However, when using the OSC protocol with OBSBOT Center, the software must be running and OSC activated.',
 		},
 		{
 			type: 'textinput',
